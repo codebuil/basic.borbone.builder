4cc8f207ad9a2cef1bd2b0f880fa0ce5c5e107b0
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-12-29 20:17:22 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+rm *.bin
 i686-w64-mingw32-as -o boot.o boot.s
 i686-w64-mingw32-gcc -c -nostdlib kernel.c -o kernel.o
-ld -nostdlib -oformat=binary -T link.ld  boot.o kernel.o -o kernel.bin 
+tcc -Wl,--oformat=binary -nostdlib -T link.ld  boot.o kernel.o -o kernel.bin 
 grub-file --is-x86-multiboot kernel.bin
 
